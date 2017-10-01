5ccaeb76c9044b310aa804399f408e0a439debb2
admin <keving_21@163.com>
2017-10-01 22:32:43 +0800

    添加 分支 测试

---
+++ src/com/keving/test/TestBranch.java
@@ -0,0 +1,14 @@
+package com.keving.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBranch {
+
+	@Test
+	public void test() {
+		System.out.println("add branch test");
+	}
+
+}
